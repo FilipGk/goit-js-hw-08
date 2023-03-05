eee92e693b0b2129fcfee83ddaa0c454b057246a
FilipGk <skomurlight@gmail.com>
2023-03-05 18:46:28 +0100

    2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime !== null) {
+  player.setCurrentTime(currentTime);
+}
